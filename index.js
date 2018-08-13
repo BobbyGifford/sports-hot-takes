45100b2835efb54ccf96273b1397f3b367d3e41c
Bobby <bobby077joe@gmail.com>
2018-08-13 16:29:17 -0700

    Adding basic backend features (login)

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ mongoose.Promise = global.Promise;
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+
 
 const app = express();
+
+// Middleware
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
