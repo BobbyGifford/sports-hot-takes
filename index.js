acabbfaa742aaa875f3f2c53a9251057091cfedc
Bobby <bobby077joe@gmail.com>
2018-08-15 14:56:10 -0700

    Preping for deployment.

--- index.js
+++ index.js
@@ -35,3 +35,13 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   `Server running on: ${PORT}`;
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
