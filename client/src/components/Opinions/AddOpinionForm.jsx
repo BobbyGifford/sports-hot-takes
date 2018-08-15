16a5a5ddd17fcddea807571ff0a0138882ebabb4
Bobby <bobby077joe@gmail.com>
2018-08-14 18:23:42 -0700

    App pulls and displays data successfully as wel as updates real time.

--- client/src/components/Opinions/AddOpinionForm.jsx
+++ client/src/components/Opinions/AddOpinionForm.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 
-const AddOpinionForm = () => {
+const AddOpinionForm = props => {
   return (
     <div className="container">
       <div className="row">
-        <form className="col s12">
+        <form className="col s12" onSubmit={props.onSubmit}>
           <div className="row">
             <div className="input-field col s12">
-              <input name="claim" id="claim" type="text" className="validate" />
+              <input
+                name="claim"
+                id="claim"
+                type="text"
+                className="validate"
+                onChange={props.onChange}
+              />
               <label htmlFor="claim">Tell me what you think</label>
             </div>
+
+            <div className="input-field col s12">
+              <input
+                name="image"
+                id="image"
+                type="text"
+                className="validate"
+                onChange={props.onChange}
+              />
+              <label htmlFor="image">Link to image</label>
+            </div>
+
             <div className="input-field col s12">
               <textarea
                 name="description"
                 id="description"
                 type="text"
                 className="materialize-textarea"
+                onChange={props.onChange}
               />
               <label htmlFor="description">Why?</label>
+            </div>
+
+            <div className="input-field col s12">
+              <input
+                className="waves-effect waves-light btn-small"
+                type="submit"
+              />
             </div>
           </div>
         </form>
--- client/src/components/Opinions/Opinions.jsx
+++ client/src/components/Opinions/Opinions.jsx
@@ -1,49 +1,101 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import * as actions from '../../actions';
 
 import Spinner from '../Spinner';
 import AddOpinionForm from './AddOpinionForm';
 
 class Opinions extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      opinions: [],
       showForm: false,
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.props.fetchOpinions();
   }
+
   renderOpinions() {
-    if (!this.props.opinions) {
-      return <Spinner />;
-    } else {
-      if (this.props.opinions.length <= 0) {
-        return (
-          <div>
-            <h1>there are no opinions</h1>
-            <p>you should add one</p>
-            <button
-              className="waves-effect waves-light btn-large"
-              onClick={() => {
-                this.setState({ showForm: !this.state.showForm });
-              }}
-            >
-              Toggle opinion form
-            </button>
-          </div>
-        );
-      } else {
-        this.setState({ opinions: this.props.opinions });
-        return <h1>opions go here</h1>;
+    //   Filters to show opinions that match current page
+    let correctOpinion = [];
+    for (let item of this.props.opinions) {
+      if (item.sport === this.props.match.params.category) {
+        correctOpinion.push(item);
       }
     }
+
+    // Displays new opinion list to page
+    return correctOpinion.map(opinion => {
+      return (
+        //   Export to component
+        <div key={opinion.claim} className="row">
+          <div className="col s12 m8 offset-m2">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">{opinion.claim}</span>
+                <p>{opinion.description}</p>
+              </div>
+              <div className="card-action">
+                <a>This is a link</a>
+                <a>This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
   }
+
+  handleInputChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = {
+      claim: this.state.claim,
+      sport: this.props.match.params.category,
+      description: this.state.description,
+      image: this.state.image,
+    };
+
+    axios.post('/api/opinions', formData).then(res => {
+      console.log(res.data);
+    });
+    this.setState({ showForm: false });
+    this.props.fetchOpinions();
+    console.log(formData);
+  }
+
   render() {
     return (
       <div className="container center-align">
         <h1>{this.props.match.params.category}</h1>
-        {this.renderOpinions()}
-        {this.state.showForm ? <AddOpinionForm /> : null}
+        {this.props.opinions ? this.renderOpinions() : <Spinner />}
+        <br />
+        <button
+          className="waves-effect waves-light btn-large"
+          onClick={() => {
+            this.setState({ showForm: !this.state.showForm });
+          }}
+        >
+          Toggle opinion form
+        </button>
+        {this.state.showForm ? (
+          <AddOpinionForm
+            onChange={this.handleInputChange}
+            onSubmit={this.handleSubmit}
+          />
+        ) : null}
       </div>
     );
   }
@@ -53,4 +105,7 @@ function mapStateToProps({ opinions }) {
   return { opinions };
 }
 
-export default connect(mapStateToProps)(Opinions);
+export default connect(
+  mapStateToProps,
+  actions
+)(Opinions);
