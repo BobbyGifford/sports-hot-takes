17e8e9cdd9b3b5a701be36e5f47863ba17d7f5df
Bobby <bobby077joe@gmail.com>
2018-09-28 18:05:25 -0700

    Better logos and images.

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import logo from '../../images/hottakeslogo.png';
+import logo from '../../images/hottakeslogo2.png';
 import './Home.css';
 
 const categories = ['NFL', 'NCAAF', 'UFC', 'NBA', 'MLB'];
